f3e53d45433f3ac5ff85b1070134c8d9bc086418
zioscuro <lore.rang@gmail.com>
2022-08-12 19:18:27 +0200

    Add getComputerChoice() function

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+function getComputerChoice() {
+    let choice = Math.floor((Math.random()*3)+1);
+    switch (choice) {
+        case 1:
+            return "Rock";
+        case 2:
+            return "Paper";
+        case 3:
+            return "Scissors";
+        default:
+            return "Something Wrong!"
+    }
+}
+
+console.log(getComputerChoice());
