4d83a53de53e5bb9616fec05524650f9e3ce2872
zioscuro <lore.rang@gmail.com>
2022-08-12 21:44:58 +0200

    Add playRound function

--- script.js
+++ script.js
@@ -1,15 +1,52 @@
 function getComputerChoice() {
-    let choice = Math.floor((Math.random()*3)+1);
+    const choice = Math.floor((Math.random()*3)+1);
     switch (choice) {
         case 1:
-            return "Rock";
+            return "rock";
         case 2:
-            return "Paper";
+            return "paper";
         case 3:
-            return "Scissors";
+            return "scissors";
         default:
-            return "Something Wrong!"
+            return "something wrong!";
     }
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    const playerChoice = playerSelection.toLowerCase();
+    if (playerChoice === computerSelection) {
+        return "It'a Draw!";
+    } 
+    if (playerChoice === "rock"){
+        if (computerSelection === "scissors"){
+            return "You Win! Rock beats Scissors";
+        }
+        if (computerSelection === "paper"){
+            return "You Lose! Paper beats Rock";
+        }
+    }
+    if (playerChoice === "paper"){
+        if (computerSelection === "rock"){
+            return "You Win! Paper beats Rock";
+        }
+        if (computerSelection === "scissors"){
+            return "You Lose! Scissors beats Paper";
+        }
+    }
+    if (playerChoice === "scissors"){
+        if (computerSelection === "paper"){
+            return "You Win! Scissors beats Paper";
+        }
+        if (computerSelection === "rock"){
+            return "You Lose! Rock beats Scissors";
+        }
+    }
+    return "something wrong!";
+  }
+
+const playerSelection = "paper";
+const computerSelection = getComputerChoice();
+
+console.log("Player: " + playerSelection)
+console.log("Computer: " + computerSelection)
+console.log(playRound(playerSelection, computerSelection));
