64348f2bbf9be8aa6eea7a1e28b9fcb20c67d0a5
zioscuro <lore.rang@gmail.com>
2022-08-25 01:09:07 +0200

    Add html buttons for selections and event listners for each button

--- script.js
+++ script.js
@@ -13,37 +13,31 @@ function getComputerChoice() {
 }
 
 function playRound(player, computer) {
-    
-    if (player.choice === computer.choice) {
+        
+    if (player === computer) {
         return 'It\'a Draw!';
     } 
-    if (player.choice === 'rock'){
-        if (computer.choice === 'scissors'){
-            player.counter += 1;
+    if (player === 'rock'){
+        if (computer === 'scissors'){            
             return 'You Win! Rock beats Scissors';
         }
-        if (computer.choice === 'paper'){
-            computer.counter += 1;
+        if (computer === 'paper'){            
             return 'You Lose! Paper beats Rock';
         }
     }
-    if (player.choice === 'paper'){
-        if (computer.choice === 'rock'){
-            player.counter += 1;
+    if (player === 'paper'){
+        if (computer === 'rock'){            
             return 'You Win! Paper beats Rock';
         }
-        if (computer.choice === 'scissors'){
-            computer.counter += 1;
+        if (computer === 'scissors'){            
             return 'You Lose! Scissors beats Paper';
         }
     }
-    if (player.choice === 'scissors'){
-        if (computer.choice === 'paper'){
-            player.counter += 1;
+    if (player === 'scissors'){
+        if (computer === 'paper'){            
             return 'You Win! Scissors beats Paper';
         }
-        if (computer.choice === 'rock'){
-            computer.counter += 1;
+        if (computer === 'rock'){            
             return 'You Lose! Rock beats Scissors';
         }
     }
@@ -88,3 +82,18 @@ function game() {
 game();
 */
 
+const rockBtn = document.getElementById('rock');
+rockBtn.addEventListener('click', ()=>{
+    const computerChoice = getComputerChoice();
+    console.log(playRound('rock', computerChoice));
+});
+const paperBtn = document.getElementById('paper');
+paperBtn.addEventListener('click', ()=>{
+    const computerChoice = getComputerChoice();
+    console.log(playRound('paper', computerChoice));
+});
+const scissorsBtn = document.getElementById("scissors")
+scissorsBtn.addEventListener('click', ()=>{
+    const computerChoice = getComputerChoice();
+    console.log(playRound('rock', computerChoice));
+});
