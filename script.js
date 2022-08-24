022f1b0fe409faf919a6f8f60ddb1f7de93ec2b3
zioscuro <lore.rang@gmail.com>
2022-08-25 00:54:24 +0200

    Comment console game function and update string declarations to single quotes for code consistency

--- script.js
+++ script.js
@@ -2,54 +2,55 @@ function getComputerChoice() {
     const choice = Math.floor((Math.random()*3)+1);
     switch (choice) {
         case 1:
-            return "rock";
+            return 'rock';
         case 2:
-            return "paper";
+            return 'paper';
         case 3:
-            return "scissors";
+            return 'scissors';
         default:
-            return "something wrong!";
+            return 'something wrong!';
     }
 }
 
 function playRound(player, computer) {
     
     if (player.choice === computer.choice) {
-        return "It'a Draw!";
+        return 'It\'a Draw!';
     } 
-    if (player.choice === "rock"){
-        if (computer.choice === "scissors"){
+    if (player.choice === 'rock'){
+        if (computer.choice === 'scissors'){
             player.counter += 1;
-            return "You Win! Rock beats Scissors";
+            return 'You Win! Rock beats Scissors';
         }
-        if (computer.choice === "paper"){
+        if (computer.choice === 'paper'){
             computer.counter += 1;
-            return "You Lose! Paper beats Rock";
+            return 'You Lose! Paper beats Rock';
         }
     }
-    if (player.choice === "paper"){
-        if (computer.choice === "rock"){
+    if (player.choice === 'paper'){
+        if (computer.choice === 'rock'){
             player.counter += 1;
-            return "You Win! Paper beats Rock";
+            return 'You Win! Paper beats Rock';
         }
-        if (computer.choice === "scissors"){
+        if (computer.choice === 'scissors'){
             computer.counter += 1;
-            return "You Lose! Scissors beats Paper";
+            return 'You Lose! Scissors beats Paper';
         }
     }
-    if (player.choice === "scissors"){
-        if (computer.choice === "paper"){
+    if (player.choice === 'scissors'){
+        if (computer.choice === 'paper'){
             player.counter += 1;
-            return "You Win! Scissors beats Paper";
+            return 'You Win! Scissors beats Paper';
         }
-        if (computer.choice === "rock"){
+        if (computer.choice === 'rock'){
             computer.counter += 1;
-            return "You Lose! Rock beats Scissors";
+            return 'You Lose! Rock beats Scissors';
         }
     }
-    return "something wrong!";
+    return 'something wrong!';
   }
 
+/* 
 function game() {
 
     const player = {
@@ -85,3 +86,5 @@ function game() {
 }
 
 game();
+*/
+
